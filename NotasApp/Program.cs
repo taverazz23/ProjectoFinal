757d15f5d384b3b0524174520714669b998d5c3f
Manuel Taveras <taverazz23@gmail.com>
2018-08-03 15:12:21 -0400

    Agrega aplicacion de Promedio de notas

--- NotasApp/Program.cs
+++ NotasApp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace NotasApp
 {
@@ -6,7 +7,45 @@ namespace NotasApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Bienvenido la aplicacion de notas");
+
+            bool continuar = true;
+            List<int> notas = new List<int>();
+
+            while (continuar) {
+                Console.WriteLine("Íngrese su nota: ");
+                string input = Console.ReadLine();
+                if (Int32.TryParse(input, out int number))
+                {
+                    notas.Add(number);
+                    Console.WriteLine("¿Termino(escriba N para terminar cualquier otra tecla y enter para continuar)? ");
+                    string stringContinuar = Console.ReadLine();
+
+                    if (stringContinuar.ToUpper() == "N")
+                        continuar = false;
+                    else
+                        continuar = true;
+                }
+                else
+                {
+                    Console.WriteLine("Valor ingresado no es numero. Hasta luego!");
+                    Environment.Exit(0);
+                }
+            }
+
+            int promedio = 0;
+            for (int i = 0; i < notas.Count; i++)
+            {
+                promedio += notas[i];
+            }
+
+            promedio = promedio / notas.Count;
+
+            Console.WriteLine("Su promedio es: " + promedio);
+
+            Console.WriteLine("Press any key to continue . . .");
+            Console.ReadLine();
+
         }
     }
 }
