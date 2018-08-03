c7d215f92426fef29aafb78d2fba012d982ac88e
Manuel Taveras <taverazz23@gmail.com>
2018-08-03 11:40:11 -0400

    Add project files.

---
+++ NotasApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NotasApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
